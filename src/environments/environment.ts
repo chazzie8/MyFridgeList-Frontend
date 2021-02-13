11155bd310f2711fbcb1cf1b4d0db62f041de3b7
Chas Richards <chas.richards@its-stuttgart.de>
2021-02-13 23:08:31 +0100

    add new apiBaseUrl for enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiBaseUrl: 'https://private-1348b9-myfridgelist.apiary-mock.com',
-  apiBaseUrl: 'http://localhost:49644/api',
+  apiBaseUrl: 'https://myfridgelist-backend.seracom.de/api',
 };
 
 /*
